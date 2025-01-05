9a82fc9dd8b802f79442ee57faa4fc67818789bc
Hadi Youssef <hmyoussef@gmail.com>
2025-01-04 20:16:01 -0500

    created landing page's design

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import './home.css'
 
 export default function Home(){
-    return(
-        <>
-            <div></div>
+    function handleClick(){
+        console.log("click");
+    }
+    return( 
+        <> 
+            <div className="centerContainer"> {/*Styled in index.css*/}
+                <div className="columnFlex"> {/*Styled in index.css*/}
+                    <h1 className="innoventure">
+                        <span className="orangeText" id="inno"> {/*Class styled in index.css*/}
+                            INNO
+                        </span>
+                        <span className="blueText" id="venture"> {/*Class styled in index.css*/}
+                            VENTURE
+                        </span>
+                    </h1>
+                    <h2 className="catchphrase">
+                        <div><span>Where </span><span className="orangeText">PROBLEMS </span><span>meet </span></div><span className="blueText">SOLUTIONS</span>
+                    </h2>
+                    <button className="btn" onClick={() => handleClick()}>Sign Up</button>
+                </div>
+            </div>
         </>
     )
 }
